1a08711729e393e0d5fe6cdccb57e557c2cb6c30
Asher Philip Samuel <ashersamuel06@gmail.com>
2021-06-23 11:16:32 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -3,11 +3,39 @@ import { createBasicUsfmEditor, withChapterPaging, withChapterSelection, withToo
 import { DemoToolbarSpecs } from '../compoents/DemoToolbarSpecs'
 
 const loading = String.raw`
-\id XXA LOADING
+\id 1TI
 \ide UTF-8
+\toc3 2SA
+\mt 1st Timothy
+\s5
 \c 1
 \p
-\v 1 Loading, please wait...
+\v 1 TESTis the beginning of the gospel of Jesus Christ, the Son of God.
+\p
+\v 2 As it is written in Isaiah the prophet,
+\q "Look, I am sending my messenger before your face,
+\q the one who will prepare your way.
+\q
+\v 3 The voice of one calling out in the wilderness,
+\q 'Make ready the way of the Lord;
+\q make his paths straight.'"
+\m
+\s5
+\p
+\v 4 John came, baptizing in the wilderness and preaching a baptism of repentance for the forgiveness of sins.
+\v 5 The whole country of Judea and all the people of Jerusalem went out to him. They were baptized by him in the Jordan River, confessing their sins.
+\v 6 John wore a coat of camel's hair and a leather belt around his waist, and he ate locusts and wild honey.
+\c 3
+\p
+\v 1 This is the beginning of the gospel of Jesus Christ, the Son of God.
+\p
+\v 2 As it is written in Isaiah the prophet,
+\q "Look, I am sending my messenger before your face,
+\q the one who will prepare your way.
+\q
+\v 3 The voice of one calling out in the wilderness,
+\q 'Make ready the way of the Lord;
+\q make his paths straight.'"
 `.trimLeft()
 
 function Home() {
